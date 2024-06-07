434d45717c33b70ad8ab1a53e383e2e74b36ae41
Raj Palande <raj.palande92@gmai.com>
2024-06-07 17:11:44 +0530

    Hello

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+        {
+        hostname: 'xifiwijfrozblcayfoge.supabase.co',
+       
+        },
+        ],
+        },
+    }
+export default nextConfig;
+
+
+
+
+
+
+
+
+
+
+
+//"xifiwijfrozblcayfoge.supabase.co"
